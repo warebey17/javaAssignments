9da2b851b08e998086f82b93818610188d3d7046
warebey17 <warebey17@gmail.com>
2016-03-07 17:25:56 -0600

    Fizzbuzz Assignment completed

---
+++ fizz.java
@@ -0,0 +1,30 @@
+
+public class fizz {
+
+	public static void main(String[] args) {
+	
+		for(int i=0; i<=100; i++){
+			
+			if (i == 0){
+				System.out.println(0);
+			}
+		else if(i%3 == 0 && i%5 == 0){
+				System.out.println("fizzbuzz");
+						}
+		else if (i%3 == 0){
+			System.out.println("fizz");		
+			}
+		else if(i%5 == 0){
+			System.out.println("buzz");
+				}
+		else{
+			System.out.println(i);
+					}
+				}
+			}
+		}
+	
+		
+	
+
+ 
